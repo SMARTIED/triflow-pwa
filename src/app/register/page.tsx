cc27f629c807a0c70db35cd5caab538a547b83b2
SMARTIED <smartied.79@gmail.com>
2026-01-12 22:49:18 +0200

    Fix register page dynamic conflict

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -6,9 +6,6 @@ const RegisterClient = dynamic(() => import("./RegisterClient"), {
   ssr: false,
 });
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
 export default function RegisterPage() {
   return <RegisterClient />;
 }
